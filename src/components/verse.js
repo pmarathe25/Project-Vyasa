9bf82989af6f0a6a28200a811278b9f7cd2159dc
Pranav Marathe <pmarathe25@gmail.com>
2022-01-31 00:00:14 -0500

    Adds word-by-word display

--- src/components/verse.js
+++ src/components/verse.js
@@ -1,15 +1,45 @@
 import * as React from 'react'
 import { useTransliterate } from './transliterationHook';
-import { verseText } from "./verse.module.css"
+import { verseText, wordByWordButton } from "./verse.module.css"
 
+const WordAndDefinition = ({ word, definition }) => {
+    word = useTransliterate(word);
+
+    console.log(word);
+    console.log(definition);
+
+    return (
+        <div className="stacked">
+            <p style={{ padding: 0 }}>
+                {word}
+            </p>
+            <p style={{ fontStyle: "italic", padding: 0 }}>
+                {definition}
+            </p>
+        </div>
+    )
+}
 
 const Verse = ({ num, text, wordByWord }) => {
     text = useTransliterate(text);
+    const [showWordByWord, setShowWordByWord] = React.useState(false);
 
     return (
-        <p className={verseText} id={`verse_${num}`}>
-            {text}
-        </p>
+        <button className={wordByWordButton} onClick={() => { setShowWordByWord(!showWordByWord) }}
+            style={{ opacity: showWordByWord ? 1.0 : 0.4 }}>
+            <div className={verseText} id={`verse_${num}`} style={{ display: "flex", justifyContent: "space-around" }}>
+                {showWordByWord
+                    ?
+                    wordByWord.map(([word, definition]) =>
+                        < WordAndDefinition word={word} definition={definition} />
+                    )
+                    :
+                    text
+                }
+            </div>
+            {/* Word By Word */}
+        </button >
+        // </div>
     )
 }
 
