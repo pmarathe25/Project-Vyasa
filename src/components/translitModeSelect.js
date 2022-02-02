7234d38051e0ce23dc84c0b4c2e531e5409f5af6
Pranav Marathe <pmarathe25@gmail.com>
2022-02-01 22:05:21 -0500

    Updates transliteration mode select to use ToggleButtonGroup

--- src/components/translitModeSelect.js
+++ src/components/translitModeSelect.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { modeSelectButton, translitModeSelect } from "./translitModeSelect.module.css"
+import { ToggleButton, ToggleButtonGroup } from "react-bootstrap";
+import { useTransliterate } from "./transliterationHook";
 
 export const TranslitModeContext = React.createContext({ mode: 0, setMode: () => { } });
 
 export const TranslitModeContextProvider = (props) => {
+    // 0: Devanagari, 1: IAST
     const [mode, setMode] = React.useState(0);
 
     return (
@@ -14,18 +16,19 @@ export const TranslitModeContextProvider = (props) => {
 }
 
 export const TransliterationModeSelect = () => {
-    // 0: Devanagari, 1: IAST
+    const devanagari = useTransliterate("daivanaagarii");
+
     return (
         <TranslitModeContext.Consumer>
             {({ mode, setMode }) =>
-                <div className={translitModeSelect}>
-                    <button className={modeSelectButton} onClick={() => { setMode(0) }} style={{ opacity: mode === 0 ? 1.0 : 0.4 }}>
-                        Devanagari
-                    </button>
-                    <button className={modeSelectButton} onClick={() => { setMode(1) }} style={{ opacity: mode === 1 ? 1.0 : 0.4 }}>
+                <ToggleButtonGroup type="radio" name="options" defaultValue={mode} onChange={(val) => { setMode(val) }} vertical={false}>
+                    <ToggleButton id="translit-select-dev" value={0}>
+                        {devanagari}
+                    </ToggleButton>
+                    <ToggleButton id="translit-select-iast" value={1}>
                         IAST
-                    </button>
-                </div >
+                    </ToggleButton>
+                </ToggleButtonGroup>
             }
         </TranslitModeContext.Consumer>
     )
