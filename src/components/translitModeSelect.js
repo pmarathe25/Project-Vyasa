3dbfe59a9fd4b6aa833908145ce246d1b4a4a266
Pranav Marathe <pmarathe25@gmail.com>
2022-03-25 23:59:38 -0400

    Makes transliteration button not change its own text

--- src/components/translitModeSelect.js
+++ src/components/translitModeSelect.js
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { Nav, ToggleButton, ToggleButtonGroup } from "react-bootstrap";
-import { useTransliterate } from "./transliterationHook";
 
 
 export const TranslitModeContext = React.createContext({ mode: 0, setMode: () => { } });
@@ -17,8 +16,6 @@ export const TranslitModeContextProvider = (props) => {
 }
 
 export const TransliterationModeSelect = ({ navExpanded }) => {
-    const devanagari = useTransliterate("daivanaagarii");
-
     const style = {
         paddingBottom: navExpanded ? "10px" : 0,
         height: "fit-content",
@@ -40,7 +37,7 @@ export const TransliterationModeSelect = ({ navExpanded }) => {
                     >
                         <ToggleButton id="translit-select-dev" value={0} size="sm">
                             <p style={{ fontSize: "16px" }}>
-                                {devanagari}
+                                देवनागरी
                             </p>
                         </ToggleButton>
                         <ToggleButton id="translit-select-iast" value={1} size="sm">
